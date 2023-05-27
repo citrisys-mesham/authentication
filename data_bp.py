4a96d450966756c321eb0ef3ae4b21c6b5cd2ec3
Muthupandian <muthupandianthangarajan@gmail.com>
2023-05-27 16:50:21 +0530

    Feature/authentication flow (#1)
    * Finished flow
    
    * Comment

---
+++ data_bp.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request, jsonify
+from constant import AUTHTOKEN
+
+api_blueprint = Blueprint("api_blueprint", __name__)
+
+@api_blueprint.before_request
+def auth():
+    authenticated = False
+    if "x-auth-token" not in request.headers:
+        return jsonify({"error": "Invalid request"}), 401
+    # This to be replaced by JWT validation
+    for auth in AUTHTOKEN:
+        if AUTHTOKEN[auth] == request.headers["x-auth-token"]:
+            authenticated = True
+            break
+    if not authenticated:
+        return jsonify({"error": "Unauthorized"}), 401
+
+
+@api_blueprint.route("/data")
+def index():
+    return [
+        "data1", "data2"
+    ]
